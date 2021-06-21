d6c4f7c649fd77e1b15a7850b3269036e726da18
Sean Wang <seanwang66@hotmail.com>
2021-06-21 22:34:13 +0800

    Initial Commit

---
+++ vaccine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  vaccine
+//
+//  Created by Sean on 2021/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
